7f47d30e04ba5402cc7197358fad7121caad49b3
李晴 <qingli@91jkys.com>
2020-12-28 13:11:36 +0800

    三级分类-配置跨域

---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.member.feign;
+
+import com.atguigu.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author Liqing
+ * @Date 2020/12/24 4:36 PM
+ * @Description
+ */
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+    @RequestMapping("/coupon/coupon/member/list")
+    public R membercoupons();
+}
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,13 +1,20 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @Author Liqing
+ * @Date 2020/12/23 6:58 PM
+ * @Description
+ */
+@EnableDiscoveryClient
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GulimallProductApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProductApplication.class, args);
+    }
 }
